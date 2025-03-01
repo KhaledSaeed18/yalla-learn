4b756c02f396a21b12170eab5d9d166afaf05b8b
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-03-01 20:36:15 +0200

    Install and configure shadcn/ui for Next.js and tailwind css v4.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-dvh">
+    <div className="flex flex-col space-y-4 items-center justify-center h-dvh">
       <h1 className="text-4xl font-bold">
         Next.js Project
       </h1>
+      <Button>
+        Click me
+      </Button>
     </div>
   );
 }
