eaf1591125af0d24ee330ee025f660c360d71e86
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-19 23:46:42 +0300

    feat(layout): add FeaturesSectionUI component to enhance feature presentation on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { FeaturesSectionUI } from "@/components/shared/FeaturesSectionUI";
 import { GlowingEffectUI } from "@/components/shared/GlowingEffectUI";
 import { MacbookScrollUi } from "@/components/shared/MacbookScrollUI";
 import { ThreeDMarqueeUI } from "@/components/shared/ThreeDMarqueeUI";
@@ -8,6 +9,7 @@ export default function Home() {
       <MacbookScrollUi />
       <main className="container mx-auto p-4">
         <GlowingEffectUI />
+        <FeaturesSectionUI />
         <ThreeDMarqueeUI />
       </main>
     </>
