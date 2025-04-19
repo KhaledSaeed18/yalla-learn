963ca94b114020c2e436df6acad4d55bc3e42098
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-19 23:42:20 +0300

    feat(layout): integrate MacbookScrollUi component for enhanced visual presentation in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import { GlowingEffectUI } from "@/components/shared/GlowingEffectUI";
+import { MacbookScrollUi } from "@/components/shared/MacbookScrollUI";
 import { ThreeDMarqueeUI } from "@/components/shared/ThreeDMarqueeUI";
 
 export default function Home() {
   return (
-    <main className="container mx-auto p-4">
-      <GlowingEffectUI />
-      <ThreeDMarqueeUI />
-    </main>
+    <>
+      <MacbookScrollUi />
+      <main className="container mx-auto p-4">
+        <GlowingEffectUI />
+        <ThreeDMarqueeUI />
+      </main>
+    </>
   );
 }
 
