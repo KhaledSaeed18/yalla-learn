3c7ffc59778128b652ac915b1e84ddc1605e3aef
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-22 12:06:12 +0300

    feat: implement AnimatedBeam and AnimatedBeamUI components for enhanced visual effects

--- app/ai-tools/page.tsx
+++ app/ai-tools/page.tsx
@@ -1,6 +1,12 @@
+import { AnimatedBeamUI } from "@/components/shared/AnimatedBeamUI"
+
 const page = () => {
     return (
-        <div>page</div>
+        <main className="flex flex-col w-full container px-4 mx-auto">
+            <div className="p-4">
+                <AnimatedBeamUI />
+            </div>
+        </main>
     )
 }
 
