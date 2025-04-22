3004d0d26ecb56596abdd6c08d1028422fb43c01
KhaledSaeed18 <khaled18saeed@gmail.com>
2025-04-22 18:03:04 +0300

    refactor: remove unused imports and clean up SVG attributes for consistency

--- components/shared/AnimatedBeamMultipleOutputUI.tsx
+++ components/shared/AnimatedBeamMultipleOutputUI.tsx
@@ -4,7 +4,7 @@ import React, { forwardRef, useRef } from "react";
 
 import { cn } from "@/lib/utils";
 import { AnimatedBeam } from "@/components/magicui/animated-beam";
-import { BracesIcon, Brain, BrainCircuit, FileText, Image, Languages, Network, SquareMousePointer } from "lucide-react";
+import { BrainCircuit, FileText, Image, Languages, Network, SquareMousePointer } from "lucide-react";
 
 const Circle = forwardRef<
     HTMLDivElement,
@@ -14,7 +14,7 @@ const Circle = forwardRef<
         <div
             ref={ref}
             className={cn(
-                "z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3 shadow-[0_0_20px_-12px_rgba(0,0,0,0.8)]",
+                "z-10 flex size-12 items-center justify-center rounded-full border-2 bg-white p-3",
                 className,
             )}
         >
